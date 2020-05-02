ef2c29bd4ef83428651b48493d6f60a42f3c1f78
Nate Petersen <natepetersen@gmail.com>
2020-05-02 00:17:59 -0600

    Week 02 Homepage assignment commit

--- web/index.php
+++ web/index.php
@@ -1,5 +1,230 @@
-<?php
+<!doctype html>
+<html lang="en">
 
-  phpinfo();
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+  <meta name="apple-mobile-web-app-capable" content="yes" />
 
-?>
+  <title>Nate Petersen - CS 313</title>
+
+  <!-- Fonts and Material Icons -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700|Material+Icons" rel="stylesheet" type="text/css" />
+
+
+  <!-- Styles -->
+  <link href="css/home.min.css" rel="stylesheet" type="text/css">
+  <link href="css/home.custom.css" rel="stylesheet" type="text/css">
+
+  <!-- Scripts -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+  <script src="js/home.min.js" type="text/javascript" name="main-scripts"></script>
+
+  <!-- Custom Scripts -->
+  <!--script src="js/home.custom.js" type="text/javascript" name="custom-scripts"></script-->
+
+</head>
+
+<body class="slides simplifiedMobile smoothScroll animated">
+
+
+  <!-- SVG Library -->
+  <svg xmlns="http://www.w3.org/2000/svg" style="display:none">
+
+    <symbol id="close" viewBox="0 0 30 30">
+      <path
+        d="M15 0c-8.3 0-15 6.7-15 15s6.7 15 15 15 15-6.7 15-15-6.7-15-15-15zm5.7 19.3c.4.4.4 1 0 1.4-.2.2-.4.3-.7.3s-.5-.1-.7-.3l-4.3-4.3-4.3 4.3c-.2.2-.4.3-.7.3s-.5-.1-.7-.3c-.4-.4-.4-1 0-1.4l4.3-4.3-4.3-4.3c-.4-.4-.4-1 0-1.4s1-.4 1.4 0l4.3 4.3 4.3-4.3c.4-.4 1-.4 1.4 0s.4 1 0 1.4l-4.3 4.3 4.3 4.3z" />
+    </symbol>
+
+    <symbol id="close-small" viewBox="0 0 11 11">
+      <path
+        d="M6.914 5.5l3.793-3.793c.391-.391.391-1.023 0-1.414s-1.023-.391-1.414 0l-3.793 3.793-3.793-3.793c-.391-.391-1.023-.391-1.414 0s-.391 1.023 0 1.414l3.793 3.793-3.793 3.793c-.391.391-.391 1.023 0 1.414.195.195.451.293.707.293s.512-.098.707-.293l3.793-3.793 3.793 3.793c.195.195.451.293.707.293s.512-.098.707-.293c.391-.391.391-1.023 0-1.414l-3.793-3.793z" />
+    </symbol>
+
+    <symbol id="arrow-left" viewBox="0 0 31 72">
+      <path
+        d="M30 72c-.3 0-.6-.1-.8-.4l-29-34c-.3-.4-.3-.9 0-1.3l29-36c.3-.4 1-.5 1.4-.2.4.3.5 1 .2 1.4l-28.5 35.5 28.5 33.4c.4.4.3 1.1-.1 1.4-.2.1-.5.2-.7.2z" />
+    </symbol>
+
+    <symbol id="arrow-right" viewBox="0 0 31 72">
+      <path
+        d="M1 0c.3 0 .6.1.8.4l29 34c.3.4.3.9 0 1.3l-29 36c-.3.4-1 .5-1.4.2-.4-.3-.5-1-.2-1.4l28.5-35.5-28.5-33.4c-.4-.4-.3-1.1.1-1.4.2-.1.5-.2.7-.2z" />
+    </symbol>
+
+    <symbol id="back" viewBox="0 0 20 20">
+      <path
+        d="M2.3 10.7l5 5c.4.4 1 .4 1.4 0s.4-1 0-1.4l-3.3-3.3h11.6c.6 0 1-.4 1-1s-.4-1-1-1h-11.6l3.3-3.3c.4-.4.4-1 0-1.4-.2-.2-.4-.3-.7-.3s-.5.1-.7.3l-5 5c-.2.2-.3.5-.3.7 0 .2.1.5.3.7z" />
+    </symbol>
+
+    <symbol id="menu" viewBox="0 0 22 22">
+      <path
+        d="M1 5h20c.6 0 1-.4 1-1s-.4-1-1-1h-20c-.6 0-1 .4-1 1s.4 1 1 1zm20 5h-20c-.6 0-1 .4-1 1s.4 1 1 1h20c.6 0 1-.4 1-1s-.4-1-1-1zm0 7h-20c-.6 0-1 .4-1 1s.4 1 1 1h20c.6 0 1-.4 1-1s-.4-1-1-1z" />
+    </symbol>
+
+    <symbol id="share" viewBox="0 0 22 22">
+      <path
+        d="M21 10c-.6 0-1 .4-1 1v7h-18v-7c0-.6-.4-1-1-1s-1 .4-1 1v7c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2v-7c0-.6-.4-1-1-1zM5.5 7.5c.3 0 .5-.1.7-.3l3.8-3.8v9.6c0 .6.4 1 1 1s1-.4 1-1v-9.6l3.8 3.8c.2.2.5.3.7.3s.5-.1.7-.3c.4-.4.4-1 0-1.4l-5.5-5.5c-.1-.1-.2-.2-.3-.2-.2-.1-.5-.1-.8 0l-.3.2-5.5 5.5c-.4.4-.4 1 0 1.4.2.2.4.3.7.3z" />
+    </symbol>
+
+    <symbol id="arrow-down" viewBox="0 0 24 24">
+      <path
+        d="M12 18c-.2 0-.5-.1-.7-.3l-11-10c-.4-.4-.4-1-.1-1.4.4-.4 1-.4 1.4-.1l10.4 9.4 10.3-9.4c.4-.4 1-.3 1.4.1.4.4.3 1-.1 1.4l-11 10c-.1.2-.4.3-.6.3z" />
+    </symbol>
+
+    <symbol id="arrow-up" viewBox="0 0 24 24">
+      <path
+        d="M11.9 5.9c.2 0 .5.1.7.3l11 10c.4.4.4 1 .1 1.4-.4.4-1 .4-1.4.1l-10.4-9.4-10.3 9.4c-.4.4-1 .3-1.4-.1-.4-.4-.3-1 .1-1.4l11-10c.1-.2.4-.3.6-.3z" />
+    </symbol>
+
+    <symbol id="arrow-top" viewBox="0 0 24 24">
+      <path
+        d="M20.7 10.3l-8-8c-.4-.4-1-.4-1.4 0l-8 8c-.4.4-.4 1 0 1.4s1 .4 1.4 0l6.3-6.3v15.6c0 .6.4 1 1 1s1-.4 1-1v-15.6l6.3 6.3c.2.2.5.3.7.3s.5-.1.7-.3c.4-.4.4-1 0-1.4z" />
+    </symbol>
+
+    <symbol id="drop-down" viewBox="0 0 16 16">
+      <polyline stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"
+        points="1,5 8,12 15,5" fill="none"></polyline>
+    </symbol>
+
+    <symbol id="play" viewBox="0 0 30 30">
+      <path d="M7 30v-30l22 15z" />
+    </symbol>
+
+    <symbol id="login" viewBox="0 0 22 22">
+      <path
+        d="M13 2c0 .6.4 1 1 1h6v16h-6c-.6 0-1 .4-1 1s.4 1 1 1h6c1.1 0 2-.9 2-2v-16c0-1.1-.9-2-2-2h-6c-.6 0-1 .4-1 1zm-6.5 3.5c0 .3.1.5.3.7l3.8 3.8h-9.6c-.6 0-1 .4-1 1s.4 1 1 1h9.6l-3.8 3.8c-.2.2-.3.5-.3.7s.1.5.3.7c.4.4 1 .4 1.4 0l5.5-5.5c.1-.1.2-.2.2-.3.1-.2.1-.5 0-.8l-.2-.3-5.5-5.5c-.4-.4-1-.4-1.4 0-.2.2-.3.4-.3.7z" />
+    </symbol>
+
+    <symbol id="chat" viewBox="0 0 22 22">
+      <path
+        d="M11 22c-.1 0-.3 0-.4-.1-.4-.1-.6-.5-.6-.9v-4h-7.8c-1.2 0-2.2-1-2.2-2.2v-11.6c0-1.2 1-2.2 2.2-2.2h17.7c1.1 0 2.1 1 2.1 2.2v11.7c0 1.2-1 2.2-2.2 2.2h-3.4l-4.7 4.7c-.2.1-.4.2-.7.2zm-8.8-19c-.1 0-.2.1-.2.2v11.7s.1.1.2.1h8.8c.6 0 1 .4 1 1v2.6l3.3-3.3c.2-.2.4-.3.7-.3h3.8c.1 0 .2-.1.2-.2v-11.6c0-.1-.1-.2-.2-.2h-17.6zM5 6h6v2h-6zM5 10h10v2h-10z" />
+    </symbol>
+
+    <symbol id="mail" viewBox="0 0 22 22">
+      <path
+        d="M19.8 2h-17.6c-1.2 0-2.2 1-2.2 2.2v13.5c0 1.3 1 2.3 2.2 2.3h17.5c1.2 0 2.2-1 2.2-2.2v-13.6c.1-1.2-.9-2.2-2.1-2.2zm-17.6 2h17.5c.2 0 .3.1.3.2v.3l-9 6.3-9-6.3v-.3c0-.1.1-.2.2-.2zm17.6 14h-17.6c-.1 0-.2-.1-.2-.2v-10.9l8.1 5.7c.3.2.6.3.9.3.3 0 .6-.1.9-.3l8.1-5.7v10.9c0 .1-.1.2-.2.2z" />
+    </symbol>
+
+    <symbol id="sound-on" viewBox="0 0 22 22">
+      <path
+        d="M15 15c-.2 0-.4-.1-.6-.2-.4-.3-.5-1-.2-1.4.5-.7.8-1.5.8-2.4s-.3-1.7-.8-2.4c-.3-.4-.2-1.1.2-1.4.4-.3 1.1-.2 1.4.2.8 1 1.2 2.3 1.2 3.6s-.4 2.6-1.2 3.6c-.2.3-.5.4-.8.4zm4 2.3c-.2 0-.5-.1-.7-.2-.4-.4-.5-1-.1-1.4 1.2-1.3 1.8-3 1.8-4.7 0-1.8-.7-3.5-1.9-4.8-.4-.4-.4-1 0-1.4s1-.4 1.4 0c1.6 1.7 2.5 3.9 2.5 6.2 0 2.2-.8 4.3-2.2 6-.2.2-.5.3-.8.3zm-9 2.7c-.2 0-.4-.1-.6-.2l-4.8-3.8h-2.1c-1.4 0-2.5-1.1-2.5-2.5v-5c0-1.4 1.1-2.5 2.5-2.5h2.2l4.7-3.8c.3-.2.7-.3 1.1-.1.3.2.5.5.5.9v16c0 .4-.2.7-.6.9-.1.1-.3.1-.4.1zm-7.5-12c-.3 0-.5.2-.5.5v5c0 .3.2.5.5.5h2.5c.2 0 .4.1.6.2l3.4 2.7v-11.8l-3.4 2.7c-.2.1-.4.2-.6.2h-2.5z" />
+    </symbol>
+
+    <symbol id="sound-off" viewBox="0 0 22 22">
+      <path
+        d="M10.434 2.098c-.347-.165-.758-.119-1.058.121l-4.726 3.781h-2.154c-1.376 0-2.496 1.12-2.496 2.496v5.009c0 1.376 1.12 2.495 2.496 2.495h2.153l4.726 3.781c.181.145.402.219.625.219.147 0 .295-.032.433-.099.347-.167.567-.516.567-.901v-16c0-.384-.22-.735-.566-.902zm-1.434 14.821l-3.375-2.7c-.178-.142-.398-.219-.625-.219h-2.504c-.274 0-.496-.222-.496-.495v-5.009c0-.274.222-.496.496-.496h2.504c.227 0 .447-.077.625-.219l3.375-2.7v11.838zM19.414 11l2.293-2.293c.391-.391.391-1.023 0-1.414s-1.023-.391-1.414 0l-2.293 2.293-2.293-2.293c-.391-.391-1.023-.391-1.414 0s-.391 1.023 0 1.414l2.293 2.293-2.293 2.293c-.391.391-.391 1.023 0 1.414.195.195.451.293.707.293s.512-.098.707-.293l2.293-2.293 2.293 2.293c.195.195.451.293.707.293s.512-.098.707-.293c.391-.391.391-1.023 0-1.414l-2.293-2.293z" />
+    </symbol>
+
+  </svg>
+
+  <!-- Navigation -->
+  <nav class="side">
+    <div class="navigation">
+      <ul></ul>
+    </div>
+  </nav>
+
+  <!-- Panel Top #08 -->
+  <nav class="panel top forceMobileView">
+    <div class="sections">
+      <div class="left"></div>
+      <div class="center"><a href="#home" title="Nate Petersen">Nate Petersen</a></div>
+      <div class="right"><span class="button actionButton sidebarTrigger" data-sidebar-id="1"><svg>
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu"></use>
+          </svg></span></div>
+    </div>
+  </nav>
+
+  <!-- Sidebar -->
+  <nav class="sidebar black" data-sidebar-id="1">
+    <div class="close"><svg>
+        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#close"></use>
+      </svg></div>
+    <div class="content">
+      <a href="#home" title="Nate Petersen" class="logo">Nate Petersen</a>
+      <ul class="mainMenu uppercase">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#assignments">Assignments</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <section name="home" class="slide fade kenBurns">
+    <div class="content">
+      <div class="container">
+        <div class="wrap">
+
+          <div class="fix-10-12">
+            <!-- <div class="h1 ae-1"></div> -->
+            <div class="ae-2">
+              <p class="hero">Hello, my name is Nate Petersen. <br> I'm a CS 313 student, and I enjoy skiing and racing bicycles.</p>
+            </div>
+            <a href="#assignments" class="button orange ae-5">See my assignments</a>
+          </div>
+          <div class="ae-10 fromRight">
+            <?php echo date("l, M j, Y") ?>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="background" style="background-image:url(images/bg-home.jpg)"></div>
+  </section>
+
+  <section name="assignments" class="slide fade kenBurns">
+    <div class="content">
+      <div class="container">
+        <div class="wrap">
+
+          <div class="fix-6-12">
+            <h1 class="ae-1">Assignments</h1>
+            <div class="ae-2">
+              <p class="hero">Nothing to see here yet.</p>
+            </div>
+            <!-- <ul class="grid fixedSpaces later equal sourceSans grid-86">
+              <li class="col-6-12 ae-2">
+                <div class="box-86 tint-86">
+                  <h3 class="small uppercase ae-4">Assignment 1</h3>
+                  <div class="ae-5"></div>
+                  <div class="features-86 equalElement ae-6">
+                    
+                  </div>
+                  <a href="#" class="button teal wide cropBottom ae-7">See assignment 1</a>
+                </div>
+              </li>
+              <li class="col-6-12 ae-3">
+                <div class="box-86">
+                  <h3 class="small uppercase ae-5">Assignment 2</h3>
+                  <div class="ae-6"></div>
+                  <div class="features-86 equalElement ae-7">
+                    
+                  </div>
+                  <a href="#" class="button pink wide cropBottom ae-8">See assignment 2</a>
+                </div>
+              </li>
+            </ul> -->
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <div class="background" style="background-image:url(images/bg-assignments.jpg)"></div>
+  </section>
+
+  <nav class="panel bottom">
+    <div class="sections">
+      <div class="center"><span class="nextSlide"><svg>
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-down"></use>
+          </svg></span></div>
+    </div>
+  </nav>
+
+  <div class="loadingIcon"><svg class="loading-icon" id="loading-circle" viewBox="0 0 18 18">
+      <circle class="circle" opacity=".1" stroke="#fff" stroke-width="2" stroke-miterlimit="10" cx="9" cy="9" r="8"
+        fill="none"></circle>
+      <circle class="dash" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" stroke-dasharray="1,100"
+        cx="9" cy="9" r="8" fill="none"></circle>
+    </svg></div>
+
+</body>
+
+</html>
