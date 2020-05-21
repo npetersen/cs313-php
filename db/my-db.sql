3f95377cad76818aad3af066f84692b3b75aa2e8
Nate Petersen <natepetersen@gmail.com>
2020-05-21 00:21:14 -0600

    Added insert statements to populate tables

--- db/my-db.sql
+++ db/my-db.sql
@@ -36,3 +36,17 @@ CREATE TABLE loanapp.loan (
     term INT,
     status VARCHAR(10) NOT NULL
 );
+
+insert into loanapp.loan_types(loan_type) values ('auto');
+insert into loanapp.loan_types(loan_type) values ('credit card');
+insert into loanapp.loan_types(loan_type) values ('rv');
+insert into loanapp.loan_types(loan_type) values ('personal');
+
+insert into loanapp.applicant (account_number, first_name, last_name, ssn, gross_monthly_income, email, phone) values (5001227, 'Lester', 'Testcase', '000-00-0001', 8000, 'lester@testcase.com', '801-778-8386');
+insert into loanapp.applicant (account_number, first_name, last_name, ssn, gross_monthly_income, email, phone) values (5001144, 'Jeremiah', 'Testcase', '000-00-0002', 10000, 'jeremiah@test.com', '801-732-0055');
+
+insert into loanapp.applicant_address (account_number, street_address, city, state, zip) values (5001227, '2801 N 1050 E', 'Ogden', 'UT', '84414');
+insert into loanapp.applicant_address (account_number, street_address, city, state, zip) values (5001144, '2879 W 3600 N', 'Ogden', 'UT', '84404');
+
+insert into loanapp.loan (loan_type, applicant, rate, amount, term, status) values (1, 5001227, 3.5, 10000, 48, 'pending');
+insert into loanapp.loan (loan_type, applicant, rate, amount, term, status) values (3, 5001144, 5, 5000, 36, 'pending');
